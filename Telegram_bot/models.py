fc715eaaf27ec9e488e7945cf2b48beed1cfcd2e
tykhon124 <tixonlamtsev@gmail.com>
2026-03-14 19:23:20 +0200

    models.py

--- Telegram_bot/models.py
+++ Telegram_bot/models.py
@@ -1,14 +1,6 @@
 import logging
 
 logger = logging.getLogger(__name__)
-logging.basicConfig(
-        level=logging.INFO,
-        format='%(asctime)s | %(levelname)s | %(name)s | %(message)s',
-        handlers=[
-            logging.FileHandler("bot_logger.log", mode="a", encoding="utf-8"),
-            logging.StreamHandler()
-        ]
-    )
 
 from pydantic import BaseModel
 
